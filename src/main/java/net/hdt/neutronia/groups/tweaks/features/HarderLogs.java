2b0418f42321126bd4a1d8f3a2f2ad31279d863d
TheGamingHuskyMC <sindrefagerheim53@gmail.com>
2018-08-27 09:35:52 +0200

    Fixed a thing

--- src/main/java/net/hdt/neutronia/groups/tweaks/features/HarderLogs.java
+++ src/main/java/net/hdt/neutronia/groups/tweaks/features/HarderLogs.java
@@ -2,7 +2,6 @@ package net.hdt.neutronia.groups.tweaks.features;
 
 import net.hdt.neutronia.base.groups.Component;
 import net.minecraft.block.BlockLog;
-import net.minecraft.item.ItemTool;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -10,7 +9,7 @@ public class HarderLogs extends Component {
 
     @SubscribeEvent
     public void onBreakBlocks(PlayerEvent.BreakSpeed event) {
-        if(!(event.getEntityPlayer().getActiveItemStack().getItem() instanceof ItemTool)) {
+        if(event.getEntityPlayer().getActiveItemStack().isEmpty()) {
             if(event.getState().getBlock() instanceof BlockLog) {
                 event.setNewSpeed(0);
             }
