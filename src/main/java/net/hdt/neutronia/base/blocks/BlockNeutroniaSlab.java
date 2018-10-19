1c1059d73322ac50f766f0bf998c4a74db2038c4
TheGamingHuskyMC <sindrefagerheim53@gmail.com>
2018-10-19 07:49:40 +0200

    A lot of textures for future blocks and items I will be adding and also updated lib mod and some classes

--- src/main/java/net/hdt/neutronia/base/blocks/BlockNeutroniaSlab.java
+++ src/main/java/net/hdt/neutronia/base/blocks/BlockNeutroniaSlab.java
@@ -8,13 +8,8 @@ import net.minecraft.creativetab.CreativeTabs;
 public class BlockNeutroniaSlab extends BlockModSlab implements INeutroniaBlock {
 
     public BlockNeutroniaSlab(String name, Material materialIn, boolean doubleSlab) {
-        super(BlockModSlab.Builder.create(materialIn), name + "_slab", doubleSlab);
+        super(name + "_slab", materialIn, doubleSlab);
         setCreativeTab(doubleSlab ? CreativeTabs.SEARCH : Neutronia.CREATIVE_TAB);
-    }
-
-    @Override
-    public String getTranslationKey(int meta) {
-        return getTranslationKey();
     }
 
 }
