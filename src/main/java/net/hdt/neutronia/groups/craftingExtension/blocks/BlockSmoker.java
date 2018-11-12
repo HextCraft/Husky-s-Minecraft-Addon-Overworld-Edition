8bb16a7e4eb53f918ad117ffa7f3f609c82ba631
TheGamingHuskyMC <sindrefagerheim53@gmail.com>
2018-11-12 15:30:37 +0100

    Added some new textures

--- src/main/java/net/hdt/neutronia/groups/craftingExtension/blocks/BlockSmoker.java
+++ src/main/java/net/hdt/neutronia/groups/craftingExtension/blocks/BlockSmoker.java
@@ -5,7 +5,9 @@ import net.hdt.neutronia.base.blocks.INeutroniaBlock;
 import net.minecraft.block.material.Material;
 
 public class BlockSmoker extends BlockMod implements INeutroniaBlock {
+
     public BlockSmoker(String name, Material materialIn, String... variants) {
         super(name, materialIn, variants);
     }
+
 }
