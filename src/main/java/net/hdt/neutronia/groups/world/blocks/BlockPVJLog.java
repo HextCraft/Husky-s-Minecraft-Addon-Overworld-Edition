bd4c94ed3bedc78b617bbed668de1b5ae39977f7
OliviaTheVampire <sindrefagerheim53@gmail.com>
2018-12-01 21:09:00 +0100

    is it fixed now?

--- src/main/java/net/hdt/neutronia/groups/world/blocks/BlockPVJLog.java
+++ src/main/java/net/hdt/neutronia/groups/world/blocks/BlockPVJLog.java
@@ -1,11 +1,8 @@
 package net.hdt.neutronia.groups.world.blocks;
 
 import net.hdt.neutronia.base.blocks.BlockNeutroniaPillar;
-import net.minecraft.block.BlockLog;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyEnum;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
@@ -14,11 +11,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
 public class BlockPVJLog extends BlockNeutroniaPillar {
-    public static final PropertyEnum<BlockLog.EnumAxis> LOG_AXIS = PropertyEnum.create("axis", BlockLog.EnumAxis.class);
 
     public BlockPVJLog(String name) {
         super(Material.WOOD, name);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(LOG_AXIS, BlockLog.EnumAxis.Y));
         setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
     }
 
@@ -35,54 +30,6 @@ public class BlockPVJLog extends BlockNeutroniaPillar {
     @Override
     public int getFireSpreadSpeed(IBlockAccess world, BlockPos pos, EnumFacing face) {
         return Blocks.LOG.getFireSpreadSpeed(world, pos, face);
-    }
-
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        IBlockState iblockstate = this.getDefaultState();
-
-        switch (meta) {
-            case 0:
-                iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.Y);
-                break;
-            case 1:
-                iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.X);
-                break;
-            case 2:
-                iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.Z);
-                break;
-            default:
-                iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.NONE);
-        }
-
-        return iblockstate;
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        int i;
-
-        switch (state.getValue(LOG_AXIS)) {
-            case Y:
-                i = 0;
-                break;
-            case X:
-                i = 1;
-                break;
-            case Z:
-                i = 2;
-                break;
-            default:
-                i = 0;
-                break;
-        }
-
-        return i;
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, LOG_AXIS);
     }
 
 }
