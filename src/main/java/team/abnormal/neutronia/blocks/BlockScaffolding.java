6faabec633f9383953f40e1ae92786c2027c6056
OliviaTheVampire <sindrefagerheim53@gmail.com>
2019-01-16 16:59:32 +0100

    Some new textures and updated old ones

---
+++ src/main/java/team/abnormal/neutronia/blocks/BlockScaffolding.java
@@ -0,0 +1,12 @@
+package team.abnormal.neutronia.blocks;
+
+import team.abnormal.neutronia.base.blocks.BlockNeutroniaBase;
+import team.abnormal.neutronia.blocks.materials.MaterialPart;
+
+public class BlockScaffolding extends BlockNeutroniaBase {
+
+    public BlockScaffolding(String name) {
+        super(name, new MaterialPart());
+    }
+
+}
